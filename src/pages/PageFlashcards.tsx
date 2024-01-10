23787c7080fa16ab6530d9c512081b7a5dd66b1c
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-11 00:30:35 +0100

    added flashcards on page via import

--- src/pages/PageFlashcards.tsx
+++ src/pages/PageFlashcards.tsx
@@ -1,5 +1,16 @@
+import db from "../../backend/data/db.json";
+
+const flashcards = db.flashcards;
+
 export const PageFlashcards = () => {
 	return (
-		<p>This is the flashcards page.</p>
-	)
-}
+		<>
+			<p>There are {flashcards.length} flashcards.</p>
+			<ul className="list-disc ml-4 mt-4">
+				{flashcards.map((flashcard) => {
+					return <li key={flashcard.suuid}>{flashcard.front}</li>;
+				})}
+			</ul>
+		</>
+	);
+};
