2fa2bcfa0f138a345820eabd3fcda184fd71109b
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-10 17:12:03 +0100

    init commit

---
+++ src/shared/interfaces.ts
@@ -0,0 +1,51 @@
+export interface INewFlashcard {
+	category: string;
+	front: string
+	back: string;
+}
+
+export const blankNewFlashcard: INewFlashcard = {
+	category: 'linux',
+	front: '',
+	back: ''
+}
+
+export interface IFlashcard extends INewFlashcard {
+	suuid: string;
+}
+
+export interface IFrontendFlashcard extends IFlashcard {
+	userIsDeleting: boolean;
+	userIsEditing: boolean;
+}
+
+export interface IPatchFlashcard {
+	category?: string;
+	front?: string
+	back?: string;
+}
+
+export interface IDatabase {
+	flashcards: IFlashcard[];
+}
+
+export interface IPromiseResolution {
+	message: string;
+}
+
+export const convertFlashcardToFrontendFlaschard = (flashcard: IFlashcard): IFrontendFlashcard => {
+	return {
+		...flashcard,
+		userIsDeleting: false,
+		userIsEditing: false
+	}
+}
+
+export const convertFrontendFlashcardToFlaschard = (frontendFlashcard: IFrontendFlashcard): IFlashcard => {
+    return {
+        suuid: frontendFlashcard.suuid,
+        category: frontendFlashcard.category,
+        front: frontendFlashcard.front,
+        back: frontendFlashcard.back
+    }
+}
